53e1cd1fd43dde478d39400d38f2c3d1d20b63b5
archana sudani <archanasudani@gmail.com>
2019-01-05 22:38:40 +0000

    My first commit

---
+++ src/loveProgramming/Print_1_To_100_UsingWhile.java
@@ -0,0 +1,15 @@
+package loveProgramming;
+
+public class Print_1_To_100_UsingWhile {
+    public static void main(String args[])
+    {
+
+        int i=1;
+        System.out.println("Output is : ");
+        while(i<=100)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
---
+++ src/loveProgramming/Print_1_To_10_UsingWhile.java
@@ -0,0 +1,15 @@
+package loveProgramming;
+
+public class Print_1_To_10_UsingWhile {
+    public static void main(String args[])
+    {
+
+        int i=1;
+        System.out.println("Output is : ");
+        while(i<=10)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
